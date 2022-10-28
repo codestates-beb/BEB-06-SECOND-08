4e0573a611f037aa8f6fa8819922b316beea307e
choijinyoung <chio0674@naver.com>
2022-10-28 17:38:10 +0900

    searchbar

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
+    const [search, setSearch] = useState("");
+    const onChange = (e) => {
+        setSearch(e.target.value)
+    }
+    return (
+        <div>
+            <div className='Search-bar'>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                    >
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Container>
+            </Navbar>
+            </div>
+        </div>
+    );
 }
 
 export default Main
