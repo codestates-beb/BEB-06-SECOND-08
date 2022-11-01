9b65914b538fd6488bea1feceba460e02b7671bd
jellymen7 <jellymen7al@gmail.com>
2022-11-01 09:03:56 +0900

    Fix:front

---
+++ client/src/components/MintNft.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import axios from "axios"
+const MintNft = ({ address }) => {
+
+    return (
+        <div>
+            <div>Wanna Mint?</div>
+            <div>IPFS</div>
+            <div>Name</div>
+            <div>Description</div>
+        </div>
+
+    )
+}
+export default MintNft;
